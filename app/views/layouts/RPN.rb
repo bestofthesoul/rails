149f02d618d88e085e12c3d69b73f833d9a2f801
bestofthesoul <bestofthesoul@gmail.com>
2016-04-06 11:19:56 +0800

    finished with clearance integrated with fb omniauth

---
+++ app/views/layouts/RPN.rb
@@ -0,0 +1,19 @@
+RPN
+
+[1 5 2 + * 3 / 4 -]
+
+RPN - calculator, this is exactly computer works on math
+
+loop from left to right
+
+1 5 2 +
+1 7    * 3 / 4 -
+1 7 *    3 / 4 -
+7 3 / 4 -
+
+7 3 / 4 - # ruby always gives us floor number result
+
+2 4 -
+
+2 4 -   # 2 minus 4 instead of 4 - 2
+[-2]
